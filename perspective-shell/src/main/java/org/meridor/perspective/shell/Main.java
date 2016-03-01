c1d388ced23694bd7ab3657ddb77ab029eab5cf6
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-03-01 21:56:04 +0300

    Ignoring Ctrl+c in shell (fixes #39)

--- perspective-shell/src/main/java/org/meridor/perspective/shell/Main.java
+++ perspective-shell/src/main/java/org/meridor/perspective/shell/Main.java
@@ -7,7 +7,29 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        registerSigIntHandler();
         Bootstrap.main(args);
+    }
+    
+    private static void registerSigIntHandler() {
+        if (signalsSupported()) {
+            try {
+                Class.forName("org.meridor.perspective.shell.misc.SigIntHandler");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Failed to register SIGINT handler. Pressing Ctrl+C may not work as expected.");
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private static boolean signalsSupported() {
+        try {
+            Class.forName("sun.misc.SignalHandler");
+            Class.forName("sun.misc.Signal");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
     
 }
