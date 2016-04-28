c9863db5e45b2706e28b2aaf2598d19ebb4b432e
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-04-28 22:07:20 +0300

    Added alternative config files storage directory (fixes #109)

--- perspective-shell/src/main/java/org/meridor/perspective/shell/interactive/PathUtils.java
+++ perspective-shell/src/main/java/org/meridor/perspective/shell/interactive/PathUtils.java
@@ -1,12 +1,27 @@
 package org.meridor.perspective.shell.interactive;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public final class PathUtils {
     
     public static Path getConfigurationDirectoryPath() {
-        return Paths.get(System.getProperty("user.home")).resolve(".perspective");
+        Path homeDirectory = Paths.get(System.getProperty("user.home"));
+        Path defaultConfigurationDirectory = homeDirectory.resolve(".perspective");
+        Optional<Path> configurationDirectoryCandidate = Stream.of(
+                homeDirectory.resolve(".config").resolve("perspective"),
+                defaultConfigurationDirectory
+        )
+                .filter(p -> Files.exists(p))
+                .findFirst();
+        return 
+                configurationDirectoryCandidate.isPresent() ?
+                    configurationDirectoryCandidate.get() :
+                    defaultConfigurationDirectory;
     }
     
     public static Path getConfigurationFilePath(String name) {
