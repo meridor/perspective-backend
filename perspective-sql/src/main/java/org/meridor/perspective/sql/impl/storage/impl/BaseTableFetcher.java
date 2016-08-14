85633ae7b8d5facd3e42a29148cf1792d706fd17
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-08-14 22:00:27 +0300

    Added caching to BaseTableFetcher (related to #140)

--- perspective-sql/src/main/java/org/meridor/perspective/sql/impl/storage/impl/BaseTableFetcher.java
+++ perspective-sql/src/main/java/org/meridor/perspective/sql/impl/storage/impl/BaseTableFetcher.java
@@ -1,5 +1,8 @@
 package org.meridor.perspective.sql.impl.storage.impl;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.meridor.perspective.sql.impl.storage.ObjectMapper;
 import org.meridor.perspective.sql.impl.storage.ObjectMapperAware;
 import org.meridor.perspective.sql.impl.storage.TableFetcher;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,6 +23,18 @@ public abstract class BaseTableFetcher<T> implements TableFetcher {
 
     @Autowired
     private ObjectMapperAware objectMapperAware;
+
+    private LoadingCache<String, Collection<T>> queryCache = CacheBuilder.newBuilder()
+            .concurrencyLevel(4)
+            .expireAfterAccess(10, TimeUnit.SECONDS)  //This can be converted to property
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .build(
+                    new CacheLoader<String, Collection<T>>() {
+                        public Collection<T> load(String any) throws Exception {
+                            return getRawData();
+                        }
+                    }
+            );
 
     protected abstract Class<T> getBeanClass();
 
@@ -44,7 +60,7 @@ public abstract class BaseTableFetcher<T> implements TableFetcher {
                     throw new IllegalArgumentException(String.format("Table \"%s\" does not contain column \"%s\"", tableName, columnName));
                 }
             });
-            Collection<T> rawEntities = getRawData();
+            Collection<T> rawEntities = queryCache.get(getTableName());
             return rawEntities.stream()
                     .filter(re -> {
                         String entityId = objectMapper.getId(re);
