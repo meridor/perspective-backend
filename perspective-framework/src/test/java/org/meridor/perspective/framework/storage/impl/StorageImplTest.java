a17ce59080325dd6a0c9fc42339d4ba845a75b7e
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-09-14 07:04:17 +0300

    More StorageImpl tests

--- perspective-framework/src/test/java/org/meridor/perspective/framework/storage/impl/StorageImplTest.java
+++ perspective-framework/src/test/java/org/meridor/perspective/framework/storage/impl/StorageImplTest.java
@@ -9,12 +9,18 @@ import org.meridor.perspective.framework.EntityGenerator;
 import org.meridor.perspective.framework.storage.ImagesAware;
 import org.meridor.perspective.framework.storage.InstancesAware;
 import org.meridor.perspective.framework.storage.ProjectsAware;
+import org.meridor.perspective.framework.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.hamcrest.Matchers.contains;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.meridor.perspective.framework.storage.StorageEvent.*;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
@@ -33,6 +39,44 @@ public class StorageImplTest {
     @Autowired
     private ProjectsAware projectsAware;
     
+    @Autowired
+    private Storage storage;
+    
+    @Test
+    public void testIsAvailable() {
+        assertThat(storage.isAvailable(), is(true));
+    }
+    
+    @Test
+    public void testGetQueue() {
+        BlockingQueue<Object> queue = storage.getQueue("test");
+        assertThat(queue, hasSize(0));
+        queue.add("test");
+        assertThat(queue, contains("test"));
+    }
+    
+    @Test
+    public void testGetLock() {
+        Lock lock = storage.getLock("test");
+        assertThat(lock.tryLock(), is(true));
+        lock.unlock();
+    }
+    
+    @Test
+    public void testInstanceOperations() {
+        Instance instance = EntityGenerator.getInstance();
+        String instanceId = instance.getId();
+        instancesAware.saveInstance(instance);
+        assertThat(instancesAware.instanceExists(instanceId), is(true));
+        assertThat(instancesAware.instanceExists("missing"), is(false));
+        assertThat(instancesAware.getInstance(instanceId), equalTo(Optional.of(instance)));
+        assertThat(instancesAware.getInstance("missing"), equalTo(Optional.empty()));
+        assertThat(instancesAware.getInstances(), contains(instance));
+        assertThat(instancesAware.getInstances(Collections.singleton(instanceId)), contains(instance));
+        instancesAware.deleteInstance(instanceId);
+        assertThat(instancesAware.instanceExists(instanceId), is(false));
+        assertThat(instancesAware.isInstanceDeleted(instanceId), is(true));
+    }
     @Test
     public void testInstanceEvents() throws Exception {
         TestEntityListener<Instance> listener = new TestEntityListener<>(); 
@@ -48,7 +92,22 @@ public class StorageImplTest {
         assertThat(listener.getPreviousEntities(), contains(null, testInstance, anotherInstance));
         assertThat(listener.getEvents(), contains(ADDED, MODIFIED, DELETED));
     }
-    
+
+    @Test
+    public void testImageOperations() {
+        Image image = EntityGenerator.getImage();
+        String imageId = image.getId();
+        imagesAware.saveImage(image);
+        assertThat(imagesAware.imageExists(imageId), is(true));
+        assertThat(imagesAware.imageExists("missing"), is(false));
+        assertThat(imagesAware.getImage(imageId), equalTo(Optional.of(image)));
+        assertThat(imagesAware.getImage("missing"), equalTo(Optional.empty()));
+        assertThat(imagesAware.getImages(), contains(image));
+        assertThat(imagesAware.getImages(Collections.singleton(imageId)), contains(image));
+        imagesAware.deleteImage(imageId);
+        assertThat(imagesAware.imageExists(imageId), is(false));
+        assertThat(imagesAware.isImageDeleted(imageId), is(true));
+    }
     @Test
     public void testImageEvents() throws Exception {
         TestEntityListener<Image> listener = new TestEntityListener<>(); 
@@ -63,6 +122,19 @@ public class StorageImplTest {
         assertThat(listener.getEntities(), contains(testImage, anotherImage, null));
         assertThat(listener.getPreviousEntities(), contains(null, testImage, anotherImage));
         assertThat(listener.getEvents(), contains(ADDED, MODIFIED, DELETED));
+    }
+
+    @Test
+    public void testProjectOperations() {
+        Project project = EntityGenerator.getProject();
+        String projectId = project.getId();
+        projectsAware.saveProject(project);
+        assertThat(projectsAware.projectExists(projectId), is(true));
+        assertThat(projectsAware.projectExists("missing"), is(false));
+        assertThat(projectsAware.getProject(projectId), equalTo(Optional.of(project)));
+        assertThat(projectsAware.getProject("missing"), equalTo(Optional.empty()));
+        assertThat(projectsAware.getProjects(), contains(project));
+        assertThat(projectsAware.getProjects(Collections.singleton(projectId)), contains(project));
     }
     
     @Test
