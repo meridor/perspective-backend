352af60cdd7dbd5efd780818c0f279724ca73352
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-10-02 18:45:19 +0300

    Added capability to build and push Docker containers from Maven build (fixes #161)

---
+++ perspective-rest/docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GC_OPTS=${GC_OPTS:-"-XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"}
+MEMORY_OPTS=${MEMORY_OPTS:-"-Xms256m -Xmx1024m"}
+NETWORK_OPTS=${NETWORK_OPTS:-"-Djava.net.preferIPv6Addresses=true"}
+MISC_OPTS=${MISC_OPTS:-""}
+
+JAVA_ARGS="$GC_OPTS $MEMORY_OPTS $NETWORK_OPTS $MISC_OPTS"
+
+java $JAVA_ARGS -jar /usr/share/perspective/perspective-rest/${project.build.finalName}.jar >> /var/log/perspective/perspective-rest.log 2>&1
---
+++ perspective-shell/docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GC_OPTS=${GC_OPTS:-"-XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"}
+MEMORY_OPTS=${MEMORY_OPTS:-"-Xmx256m"}
+NETWORK_OPTS=${NETWORK_OPTS:-"-Djava.net.preferIPv6Addresses=true"}
+MISC_OPTS=${MISC_OPTS:-""}
+
+JAVA_ARGS="$GC_OPTS $MEMORY_OPTS $NETWORK_OPTS $MISC_OPTS"
+
+java $JAVA_ARGS -jar /usr/share/perspective/perspective-docker/${project.build.finalName}.jar >> /var/log/perspective/perspective-docker.log 2>&1
