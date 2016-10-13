7bc0a3d7708b9874755c0df306af8eac811703f0
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-10-13 07:16:41 +0300

    Not showing useless quota information, e.g. infinite or zero values (related to #159)

--- perspective-worker/src/main/java/org/meridor/perspective/worker/misc/impl/ValueUtils.java
+++ perspective-worker/src/main/java/org/meridor/perspective/worker/misc/impl/ValueUtils.java
@@ -3,18 +3,28 @@ package org.meridor.perspective.worker.misc.impl;
 public final class ValueUtils {
     
     public static String formatQuota(Integer currentValue, Integer maxValue) {
-        if (currentValue == null && maxValue == null) {
+        if (isMeaningless(currentValue, maxValue)) {
             return null;
         }
         return String.format("%s/%s", formatValue(currentValue), formatValue(maxValue));
     }
 
+    private static boolean isMeaningless(Integer currentValue, Integer maxValue) {
+        return 
+                currentValue == null ||
+                currentValue == -1 ||
+                (
+                        maxValue != null && 
+                        ( 
+                            maxValue == -1 ||
+                            (currentValue == 0 && maxValue == 0)
+                        )
+                );
+    }
+
     private static String formatValue(Integer value){
         if (value == null) {
             return "?";
-        }
-        if (value == -1) {
-            return "inf";
         }
         return String.valueOf(value);
     }
--- perspective-worker/src/test/java/org/meridor/perspective/worker/misc/impl/ValueUtilsTest.java
+++ perspective-worker/src/test/java/org/meridor/perspective/worker/misc/impl/ValueUtilsTest.java
@@ -11,8 +11,13 @@ public class ValueUtilsTest {
     @Test
     public void testFormatQuota() {
         assertThat(formatQuota(null, null), equalTo(null));
-        assertThat(formatQuota(null, -1), equalTo("?/inf"));
+        assertThat(formatQuota(null, -1), equalTo(null));
+        assertThat(formatQuota(0, 0), equalTo(null));
+        assertThat(formatQuota(0, -1), equalTo(null));
+        assertThat(formatQuota(-1, -1), equalTo(null));
+        assertThat(formatQuota(-1, 42), equalTo(null));
         assertThat(formatQuota(1, null), equalTo("1/?"));
+        assertThat(formatQuota(0, 45), equalTo("0/45"));
         assertThat(formatQuota(42, 45), equalTo("42/45"));
     }
 
