4618eaf3d75515b2e0bfccbbd2bc1fd10bc12872
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-12-15 07:39:05 +0300

    Fixed race condition in operation failure listener test

--- perspective-worker/src/test/java/org/meridor/perspective/worker/processor/event/BaseOperationFailureTest.java
+++ perspective-worker/src/test/java/org/meridor/perspective/worker/processor/event/BaseOperationFailureTest.java
@@ -2,29 +2,25 @@ package org.meridor.perspective.worker.processor.event;
 
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.rules.Timeout;
 import org.meridor.perspective.backend.messaging.Message;
 import org.meridor.perspective.backend.messaging.TestStorage;
 import org.meridor.perspective.beans.DestinationName;
 import org.meridor.perspective.beans.Letter;
 import org.meridor.perspective.common.events.EventBus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.io.Serializable;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @ContextConfiguration(locations = "/META-INF/spring/failure-listener-context.xml")
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 public abstract class BaseOperationFailureTest {
 
     @ClassRule
@@ -33,6 +29,9 @@ public abstract class BaseOperationFailureTest {
     @Rule
     public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
+    @Rule
+    public Timeout timeout = new Timeout(5, SECONDS);
+    
     @Autowired
     private EventBus eventBus;
 
@@ -40,16 +39,10 @@ public abstract class BaseOperationFailureTest {
     private TestStorage storage;
 
     void assertListenerWorks(Message notProcessedMessage) throws Exception {
-        //Here's how we wait for event to propagate to listeners
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        eventBus.addListener(MessageNotProcessedEvent.class, event -> countDownLatch.countDown());
-        
         eventBus.fire(new MessageNotProcessedEvent(notProcessedMessage));
-        countDownLatch.await(1, SECONDS);
         
         BlockingQueue<Object> queue = storage.getQueue(DestinationName.MAIL.value());
-        assertThat(queue, hasSize(1));
-        Object messageWithLetterObject = queue.poll(100, MILLISECONDS);
+        Object messageWithLetterObject = queue.take();
         assertThat(messageWithLetterObject, is(instanceOf(Message.class)));
         Message messageWithLetter = (Message) messageWithLetterObject;
         assertThat(messageWithLetter.getCloudType(), is(nullValue()));
