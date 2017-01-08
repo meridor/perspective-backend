10b850ee480bd4327dafd81f11d11ad4a86ccf23
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-01-08 21:14:13 +0300

    Fixed logging logic in Docker containers

--- perspective-docker/docker/entrypoint.sh
+++ perspective-docker/docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 JAVA_ARGS="$GC_OPTS $MEMORY_OPTS $NETWORK_OPTS $MISC_OPTS"
-
+if [ -n "$LOGFILE" ]; then
+    LOGGING_OPTS=" >> $LOGFILE"
+fi
 java $JAVA_ARGS -jar /usr/share/perspective/perspective-docker/${project.build.finalName}.jar $LOGGING_OPTS 2>&1
